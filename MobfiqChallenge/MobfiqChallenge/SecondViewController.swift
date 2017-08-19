729731268c1f2791cd0e7517f5d2091e6d731775
eltonc.dev <eltonc.dev@gmail.com>
2017-08-18 21:25:17 -0300

    Commit inicial

---
+++ MobfiqChallenge/MobfiqChallenge/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MobfiqChallenge
+//
+//  Created by Elton Coelho on 18/08/17.
+//  Copyright © 2017 Elton Coelho. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
